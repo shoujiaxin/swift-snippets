124966db1094482a0248a991b162f4b0f8781c6c
Jiaxin Shou <shoujiaxin@gmail.com>
2022-06-19 00:00:07 +0800

    feat: add SwiftCharts initial project

---
+++ SwiftCharts/SwiftCharts/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftCharts
+//
+//  Created by Jiaxin Shou on 2022/6/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftCharts/SwiftCharts/SwiftChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsApp.swift
+//  SwiftCharts
+//
+//  Created by Jiaxin Shou on 2022/6/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
