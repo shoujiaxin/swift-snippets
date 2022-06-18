8a5f27f4d74fac16413c49f8e08e95ed61f0e180
Jiaxin Shou <shoujiaxin@gmail.com>
2022-06-19 01:28:05 +0800

    feat: add SwiftUINavigation initial project

---
+++ SwiftUINavigation/SwiftUINavigation/SwiftUINavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationApp.swift
+//  SwiftUINavigation
+//
+//  Created by Jiaxin Shou on 2022/6/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
