7fbb0f612d78083d3d2f87cf37bf96ab0247d5e4
Jiaxin Shou <shoujiaxin@gmail.com>
2022-06-19 15:23:54 +0800

    feat: finish SwiftUINavigation project

--- SwiftUINavigation/SwiftUINavigation/ContentView.swift
+++ SwiftUINavigation/SwiftUINavigation/ContentView.swift
@@ -7,13 +7,88 @@
 
 import SwiftUI
 
+enum NavigationType: String, Hashable {
+    case dm = "DM VIEW"
+    case camera = "CAMERA VIEW"
+    case home = "HOME"
+}
+
 struct ContentView: View {
+    @State private var mainStack: [NavigationType] = []
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack(path: $mainStack) {
+            TabView {
+                Text("Home")
+                    .tabItem {
+                        Image(systemName: "house.fill")
+                    }
+
+                Text("Search")
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                    }
+
+                Text("Liked")
+                    .tabItem {
+                        Image(systemName: "suit.heart.fill")
+                    }
+
+                Text("Settings")
+                    .tabItem {
+                        Image(systemName: "gearshape")
+                    }
+            }
+            .navigationTitle("Instagram")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        mainStack.append(.dm)
+                    } label: {
+                        Image(systemName: "paperplane")
+                            .font(.callout)
+                    }
+                }
+
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        mainStack.append(.camera)
+                    } label: {
+                        Image(systemName: "camera")
+                            .font(.callout)
+                    }
+                }
+            }
+            .navigationDestination(for: NavigationType.self) { value in
+                switch value {
+                case .camera:
+                    VStack {
+                        Text("Camera View")
+
+                        Button("Pop") {
+                            mainStack.removeLast()
+                        }
+
+                        Button("Go to DM View") {
+                            mainStack.append(.dm)
+                        }
+                    }
+                case .home: Text("Home View")
+                case .dm:
+                    VStack {
+                        Text("DM View")
+                    }
+
+                    Button("Pop") {
+                        mainStack.removeLast()
+                    }
+
+                    Button("Pop to root") {
+                        mainStack.removeAll()
+                    }
+                }
+            }
         }
     }
 }
