d4acc617765d7c9426efbe2782cef80bb7700260
Jiaxin Shou <shoujiaxin@gmail.com>
2022-06-20 00:34:29 +0800

    feat: add PullToRefresh initial project

---
+++ PullToRefresh/PullToRefresh/PullToRefreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  PullToRefreshApp.swift
+//  PullToRefresh
+//
+//  Created by Jiaxin Shou on 2022/6/20.
+//
+
+import SwiftUI
+
+@main
+struct PullToRefreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
