6b881fb317c039e88c8567ec16fa08b7523d1ca4
Jiaxin Shou <shoujiaxin@gmail.com>
2022-06-19 15:36:03 +0800

    feat: add Tags initial project

---
+++ Tags/Tags/TagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TagsApp.swift
+//  Tags
+//
+//  Created by Jiaxin Shou on 2022/6/19.
+//
+
+import SwiftUI
+
+@main
+struct TagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
