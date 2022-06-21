fb4eb6a1034e5b47fa20bc9beaecc219588026c1
Jiaxin Shou <shoujiaxin@gmail.com>
2022-06-21 23:31:38 +0800

    4

---
+++ PullToRefresh/PullToRefresh/View/CustomRefreshView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomRefreshView.swift
+//  PullToRefresh
+//
+//  Created by Jiaxin Shou on 2022/6/21.
+//
+
+import SwiftUI
+
+struct CustomRefreshView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomRefreshView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomRefreshView()
+    }
+}
