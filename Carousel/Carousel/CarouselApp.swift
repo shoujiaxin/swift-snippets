79d4ae5b16fb119e48208d44b1e66998c7d9828b
Jiaxin Shou <shoujiaxin@gmail.com>
2022-07-11 00:32:02 +0800

    feat: add Carousel initial project

---
+++ Carousel/Carousel/CarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselApp.swift
+//  Carousel
+//
+//  Created by Jiaxin Shou on 2022/7/11.
+//
+
+import SwiftUI
+
+@main
+struct CarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
