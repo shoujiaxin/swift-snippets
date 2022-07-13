4c89fd933e698ad845ce06c4b43c4ea419df0cf0
Jiaxin Shou <shoujiaxin@gmail.com>
2022-07-13 22:56:02 +0800

    feat: add InfiniteStackedCarousel initial project

---
+++ InfiniteStackedCarousel/InfiniteStackedCarousel/InfiniteStackedCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteStackedCarouselApp.swift
+//  InfiniteStackedCarousel
+//
+//  Created by Jiaxin Shou on 2022/7/13.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteStackedCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
