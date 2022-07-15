c42f7861f7b40dbc8e6f08678ffb7ab04377bca4
Jiaxin Shou <shoujiaxin@gmail.com>
2022-07-15 20:45:15 +0800

    feat: add DynamicTabIndicator initial project

---
+++ DynamicTabIndicator/DynamicTabIndicator/DynamicTabIndicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicTabIndicatorApp.swift
+//  DynamicTabIndicator
+//
+//  Created by Jiaxin Shou on 2022/7/15.
+//
+
+import SwiftUI
+
+@main
+struct DynamicTabIndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
