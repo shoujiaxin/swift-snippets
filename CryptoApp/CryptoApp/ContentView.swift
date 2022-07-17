25e958e62503e215a543e59f4836e05cf35dd5b8
Jiaxin Shou <shoujiaxin@gmail.com>
2022-07-17 17:06:25 +0800

    feat: add CryptoApp initial project

---
+++ CryptoApp/CryptoApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  CryptoApp
+//
+//  Created by Jiaxin Shou on 2022/7/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptoApp/CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Jiaxin Shou on 2022/7/17.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
