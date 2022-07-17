d875d6166cda8af47ae91b3e8229e54f534d7efe
Jiaxin Shou <shoujiaxin@gmail.com>
2022-07-17 20:35:24 +0800

    feat: add CustomLineGraph initial project

---
+++ CustomLineGraph/CustomLineGraph/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomLineGraph
+//
+//  Created by Jiaxin Shou on 2022/7/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomLineGraph/CustomLineGraph/CustomLineGraphApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomLineGraphApp.swift
+//  CustomLineGraph
+//
+//  Created by Jiaxin Shou on 2022/7/17.
+//
+
+import SwiftUI
+
+@main
+struct CustomLineGraphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
