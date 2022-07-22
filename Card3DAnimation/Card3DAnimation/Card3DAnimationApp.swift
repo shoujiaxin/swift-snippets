d3c725446284040d736bbabe0e64477d6616fbe8
Jiaxin Shou <shoujiaxin@gmail.com>
2022-07-22 15:09:55 +0800

    feat: add Card3DAnimation initial project

---
+++ Card3DAnimation/Card3DAnimation/Card3DAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card3DAnimationApp.swift
+//  Card3DAnimation
+//
+//  Created by Jiaxin Shou on 2022/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Card3DAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Card3DAnimation/Card3DAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Card3DAnimation
+//
+//  Created by Jiaxin Shou on 2022/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
