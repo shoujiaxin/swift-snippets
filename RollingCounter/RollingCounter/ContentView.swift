0bf290479ffe55528169054c120ea80df8e9a44f
Jiaxin Shou <shoujiaxin@gmail.com>
2022-07-23 00:17:08 +0800

    feat: add RollingCounter initial project

---
+++ RollingCounter/RollingCounter/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  RollingCounter
+//
+//  Created by Jiaxin Shou on 2022/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RollingCounter/RollingCounter/RollingCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollingCounterApp.swift
+//  RollingCounter
+//
+//  Created by Jiaxin Shou on 2022/7/23.
+//
+
+import SwiftUI
+
+@main
+struct RollingCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
