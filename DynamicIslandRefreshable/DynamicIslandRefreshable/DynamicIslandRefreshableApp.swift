65f99e6ac5ad1fc9b6720427dcc62423b1a058bc
Jiaxin Shou <shoujiaxin@gmail.com>
2022-09-28 23:39:08 +0800

    feat: add DynamicIslandRefreshable initial project

---
+++ DynamicIslandRefreshable/DynamicIslandRefreshable/DynamicIslandRefreshableApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicIslandRefreshableApp.swift
+//  DynamicIslandRefreshable
+//
+//  Created by Jiaxin Shou on 2022/9/28.
+//
+
+import SwiftUI
+
+@main
+struct DynamicIslandRefreshableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
