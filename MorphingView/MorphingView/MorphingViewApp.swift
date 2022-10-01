b22b29c20bc69fa65627f66a2a07f2ca68d6e897
Jiaxin Shou <shoujiaxin@gmail.com>
2022-10-01 22:23:40 +0800

    feat: add MorphingView initial project

---
+++ MorphingView/MorphingView/MorphingViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorphingViewApp.swift
+//  MorphingView
+//
+//  Created by Jiaxin Shou on 2022/10/1.
+//
+
+import SwiftUI
+
+@main
+struct MorphingViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
