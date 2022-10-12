9ed7ce04db003602a6afbc25f7dd5bd94c3807e4
Jiaxin Shou <shoujiaxin@gmail.com>
2022-10-12 22:35:06 +0800

    feat: add CustomSidebar initial project

---
+++ CustomSidebar/CustomSidebar/CustomSidebarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSidebarApp.swift
+//  CustomSidebar
+//
+//  Created by Jiaxin Shou on 2022/10/12.
+//
+
+import SwiftUI
+
+@main
+struct CustomSidebarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
