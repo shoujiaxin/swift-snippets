988d96bd55d257154c0f1217aa38aeb04b795873
Jiaxin Shou <shoujiaxin@gmail.com>
2022-12-18 20:03:47 +0800

    feat: add ResponsiveUI initial project & assets

---
+++ ResponsiveUI/ResponsiveUI/ResponsiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResponsiveUIApp.swift
+//  ResponsiveUI
+//
+//  Created by Jiaxin Shou on 2022/12/18.
+//
+
+import SwiftUI
+
+@main
+struct ResponsiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
