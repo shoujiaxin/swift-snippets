2b360e04545daf130b74fdca655ca9c4bfcc5f74
Jiaxin Shou <shoujiaxin@gmail.com>
2023-01-07 23:03:46 +0800

    feat: add TaskPlanner initial project

---
+++ TaskPlanner/TaskPlanner/TaskPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskPlannerApp.swift
+//  TaskPlanner
+//
+//  Created by Jiaxin Shou on 2023/1/7.
+//
+
+import SwiftUI
+
+@main
+struct TaskPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
