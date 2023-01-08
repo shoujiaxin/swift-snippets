fad18829ec101971898fe88a6c115bed7bcca422
Jiaxin Shou <shoujiaxin@gmail.com>
2023-01-08 18:16:44 +0800

    feat: add AutoOtp initial project

---
+++ AutoOtp/AutoOtp/AutoOtpApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoOtpApp.swift
+//  AutoOtp
+//
+//  Created by Jiaxin Shou on 2023/1/8.
+//
+
+import SwiftUI
+
+@main
+struct AutoOtpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
