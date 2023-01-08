dbd8baef05cf9c64cf009a2f0ffb8d4b35eaec39
Jiaxin Shou <shoujiaxin@gmail.com>
2023-01-08 18:49:27 +0800

    feat: add AnimatedColorPicker initial project

---
+++ AnimatedColorPicker/AnimatedColorPicker/AnimatedColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedColorPickerApp.swift
+//  AnimatedColorPicker
+//
+//  Created by Jiaxin Shou on 2023/1/8.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
