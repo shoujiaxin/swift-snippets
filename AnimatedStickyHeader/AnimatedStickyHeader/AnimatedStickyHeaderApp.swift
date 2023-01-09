107e5854172ae833c9a965633637ed15d0007e22
Jiaxin Shou <shoujiaxin@gmail.com>
2023-01-09 12:33:44 +0800

    feat: add AnimatedStickyHeader initial project

---
+++ AnimatedStickyHeader/AnimatedStickyHeader/AnimatedStickyHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedStickyHeaderApp.swift
+//  AnimatedStickyHeader
+//
+//  Created by Jiaxin Shou on 2023/1/9.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedStickyHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
