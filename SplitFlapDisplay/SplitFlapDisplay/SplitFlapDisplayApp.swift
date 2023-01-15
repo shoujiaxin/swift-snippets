7ceb629cd942b793ec2f3b34b3a89e8e39d775b1
Jiaxin Shou <shoujiaxin@gmail.com>
2023-01-15 13:46:45 +0800

    feat: add SplitFlapDisplay initial project

---
+++ SplitFlapDisplay/SplitFlapDisplay/SplitFlapDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitFlapDisplayApp.swift
+//  SplitFlapDisplay
+//
+//  Created by Jiaxin Shou on 2023/1/15.
+//
+
+import SwiftUI
+
+@main
+struct SplitFlapDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
