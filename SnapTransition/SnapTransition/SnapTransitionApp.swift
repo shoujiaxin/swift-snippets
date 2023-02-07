a2f0e73fdef67c52464dd9bdb11164ad5a081501
Jiaxin Shou <shoujiaxin@gmail.com>
2023-02-07 23:11:10 +0800

    feat: add SnapTransition initial project

---
+++ SnapTransition/SnapTransition/SnapTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapTransitionApp.swift
+//  SnapTransition
+//
+//  Created by Jiaxin Shou on 2023/2/6.
+//
+
+import SwiftUI
+
+@main
+struct SnapTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
