19a34550063f7531f1116fbd56ed9fcdb2ebf043
Jiaxin Shou <shoujiaxin@gmail.com>
2023-03-01 23:10:13 +0800

    feat: add CustomKeyboard initial project

---
+++ CustomKeyboard/CustomKeyboard/CustomKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomKeyboardApp.swift
+//  CustomKeyboard
+//
+//  Created by Jiaxin Shou on 2023/3/1.
+//
+
+import SwiftUI
+
+@main
+struct CustomKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
