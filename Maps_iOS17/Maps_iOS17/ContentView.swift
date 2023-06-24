4be3a2ec6c679edeeb5dcf6ed2c9489ad4deb629
Jiaxin Shou <shoujiaxin@gmail.com>
2023-06-24 23:29:48 +0800

    2

--- Maps_iOS17/Maps_iOS17/ContentView.swift
+++ Maps_iOS17/Maps_iOS17/ContentView.swift
@@ -5,17 +5,26 @@
 //  Created by Jiaxin Shou on 2023/6/24.
 //
 
+import MapKit
 import SwiftUI
 
+extension CLLocationCoordinate2D {
+    static let myLocation: CLLocationCoordinate2D = .init(latitude: 37.3346,
+                                                          longitude: -122.0090)
+}
+
+extension MKCoordinateRegion {
+    static let myRegion: MKCoordinateRegion = .init(center: .myLocation,
+                                                    latitudinalMeters: 10000,
+                                                    longitudinalMeters: 10000)
+}
+
 struct ContentView: View {
+    @State
+    private var cameraPosition: MapCameraPosition = .region(.myRegion)
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Map(position: $cameraPosition)
     }
 }
 
