0aa64cf63fdd4a576037c552cacf8f2a5db71f54
Jiaxin Shou <shoujiaxin@gmail.com>
2023-06-24 23:26:43 +0800

    1

---
+++ Maps_iOS17/Maps_iOS17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Maps_iOS17
+//
+//  Created by Jiaxin Shou on 2023/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Maps_iOS17/Maps_iOS17/Maps_iOS17App.swift
@@ -0,0 +1,17 @@
+//
+//  Maps_iOS17App.swift
+//  Maps_iOS17
+//
+//  Created by Jiaxin Shou on 2023/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Maps_iOS17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
